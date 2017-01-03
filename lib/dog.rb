69f56e6e4b8aa3af335d39cd361fcacf12ff55fc
Kevin Adkins <kevinladkins@gmail.com>
2017-01-03 17:11:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  puts "I am a dog"
+end
+
+fido = Dog.new
+lassie = Dog.new
+snoopy = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  puts "I am a person"
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
