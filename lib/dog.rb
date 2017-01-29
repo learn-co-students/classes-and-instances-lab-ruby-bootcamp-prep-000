4297d63b33d9b1cffc061adf1306f100f1d9389e
Matthew Ahern <matt.r.ahern@gmail.com>
2017-01-29 17:05:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  qualities = ["canine", "has 4 legs", "good pet"]
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  qualities = ["homosapien", "has 2 legs", "good for friends and enemies"]
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
