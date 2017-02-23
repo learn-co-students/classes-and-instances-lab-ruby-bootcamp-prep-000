7f06ecc3f29800d8216f032237fe494ca3800bcc
Mike Merin <mikemerinweather@gmail.com>
2017-02-23 15:11:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+
+
+end
+
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+
+# p feedback for anyone reading this, it may be a good idea to first introduce something simple like Time.new to get newcomers used to what a class is and what "new" does to it
