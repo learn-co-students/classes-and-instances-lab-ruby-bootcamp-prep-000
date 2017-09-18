25ffcfa36b5b9181d3ca48331a26720361da3d9c
yukkkieee <yuk.mei.ng@gmail.com>
2017-09-18 05:46:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+#DEFINE DOG
+class Dog
+end
+#MAKE 3 DOGS
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+#DEFINE PERSON
+class Person
+end
+#MAKE 2 PEOPLE
+adele_goldberg = Person.new
+alan_kay = Person.new
