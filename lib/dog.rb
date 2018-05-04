4bcb8347f4063d7b9774b9568612ecffa9d1820a
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-04 04:17:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,8 @@
+#create Dog class
 class Dog
 end
 
+#create 3 instances of the Dog class
 fido=Dog.new
 snoopy=Dog.new
 lassie=Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,8 @@
+
+#create Person class
 class Person
 end
 
+#2 instances of Person class
 adele_goldberg=Person.new
 alan_kay=Person.new
