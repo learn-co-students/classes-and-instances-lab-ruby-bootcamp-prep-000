4679ba9d2c3bd3f9ce8914086ccbed76ce615ae6
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-02 14:18:28 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  #I don't think we're describing Dog today
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  #I don't think we're describing Person today
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
