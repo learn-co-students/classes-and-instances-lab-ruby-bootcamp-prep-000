621c0fb9cfe89001c2aa7336622855740cd7bb48
stubrew24 <sewelly@gmail.com>
2019-01-16 22:23:42 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  # Dog blueprint
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  #Person blueprint
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
