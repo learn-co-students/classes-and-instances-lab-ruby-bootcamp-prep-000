3e8ba310fbf68449be2f85808443b6216946a575
Patrick Pierre <patrick.pierre000@gmail.com>
2020-06-18 22:35:15 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  #something about the Dog
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  #something about the Person
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
